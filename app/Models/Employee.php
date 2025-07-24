2604bbf8b5e8f699ec23477b48fc3994839ff07f
KayDeeGee <kylebryanbasco@gmail.com>
2025-07-24 16:20:44 +0800

    added soft deletes to model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
     //
+    use SoftDeletes;
+
     protected $fillable = [
         'user_id',
         'employee_number',
