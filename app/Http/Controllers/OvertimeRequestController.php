352fbda21e9cabf2fca6fb06b12635d9665ea5e9
KayDeeGee <kylebryanbasco@gmail.com>
2025-08-21 05:03:30 +0800

    add: store overtime request

--- app/Http/Controllers/OvertimeRequestController.php
+++ app/Http/Controllers/OvertimeRequestController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreOvertimeRequest;
 use App\Models\OvertimeRequest;
+use Illuminate\Contracts\Cache\Store;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OvertimeRequestController extends Controller
 {
@@ -24,11 +28,38 @@ class OvertimeRequestController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreOvertimeRequest $request): JsonResponse
     {
-        //
-    }
+        $validated = $request->validated();
 
+        try {
+            DB::beginTransaction();
+
+            $overtime = OvertimeRequest::create([
+                'employee_id' => 10, // static for now
+                'duration' => $validated['hours'],
+                'reason' => $validated['reason'],
+                'status' => 'pending',
+                'reviewed_by' => null,
+                'reviewed_at' => null,
+                'created_at' => $validated['date'], // date of requested OT
+            ]);
+
+            DB::commit();
+
+            return response()->json([
+                'message' => 'Overtime request submitted successfully.',
+                'data' => $overtime,
+            ], 201);
+        } catch (\Throwable $e) {
+            DB::rollBack();
+
+            return response()->json([
+                'message' => 'Failed to submit overtime request.',
+                'error'   => $e->getMessage(), // ⚠️ optional, remove in production
+            ], 500);
+        }
+    }
     /**
      * Display the specified resource.
      */
